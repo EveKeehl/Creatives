2e045408d64c6cd294bf0c2987f4241cef71ea2c
Eve Keehl <47083007+EveKeehl@users.noreply.github.com>
2020-07-17 09:53:31 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,2 @@
+$('a[href="#"]').on("click",(function(n){n.preventDefault()})),$((function(){$("#nav_menu").slicknav({prependTo:"#mobilemenu"})})),(new WOW).init();
+//# sourceMappingURL=main.js.map
